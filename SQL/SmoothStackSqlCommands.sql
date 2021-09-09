3cb3e28d2e5974d515b40fc5f11f36c22a2989d7
DavidCas89 <89817775+DavidCas89@users.noreply.github.com>
2021-09-09 19:26:48 -0400

    SQL

---
+++ SQL/SmoothStackSqlCommands.sql
@@ -0,0 +1,7 @@
+#SELECT noOfCopies FROM ( (tbl_book NATURAL JOIN tbl_book_copies ) NATURAL JOIN tbl_library_branch) WHERE title='The Lost Tribe' AND branchName='Sharpstown';
+#SELECT noOfCopies, branchName FROM ( (tbl_book NATURAL JOIN tbl_book_copies ) NATURAL JOIN tbl_library_branch) WHERE title='The Lost Tribe';
+#SELECT name FROM tbl_borrower B WHERE cardNo NOT IN ( SELECT cardNo FROM tbl_book_loans L WHERE(B.cardNo = L.cardNo)) ;
+#SELECT title,name,address FROM (( (tbl_borrower NATURAL JOIN tbl_book_loans ) NATURAL JOIN tbl_library_branch) NATURAL JOIN tbl_book) WHERE  branchName='Sharpstown' AND DATE(dueDate)='2018-04-01' ;
+#SELECT branchName,Count(*) FROM tbl_book_loans B,tbl_library_branch L WHERE  B.branchID = L.branchId GROUP BY L.branchName;
+#SELECT name,address,Count(*) FROM tbl_book_loans NATURAL JOIN tbl_borrower GROUP BY cardNo HAVING count(*)> 5;
+#SELECT title,noOfCopies FROM ( ((tbl_book NATURAL JOIN tbl_author) NATURAL JOIN tbl_book_copies ) NATURAL JOIN tbl_library_branch) WHERE authorName = "Stephen King" AND branchName='Central';
