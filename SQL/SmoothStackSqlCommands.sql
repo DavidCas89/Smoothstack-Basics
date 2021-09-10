62c4a1da70c200b6d9e3a47267638c60a89a3f10
DavidCas89 <89817775+DavidCas89@users.noreply.github.com>
2021-09-10 13:01:43 -0400

    SQL Query 7 Update

--- SQL/SmoothStackSqlCommands.sql
+++ SQL/SmoothStackSqlCommands.sql
@@ -4,4 +4,4 @@
 #SELECT title,name,address FROM (( (tbl_borrower NATURAL JOIN tbl_book_loans ) NATURAL JOIN tbl_library_branch) NATURAL JOIN tbl_book) WHERE  branchName='Sharpstown' AND DATE(dueDate)='2018-04-01' ;
 #SELECT branchName,Count(*) FROM tbl_book_loans B,tbl_library_branch L WHERE  B.branchID = L.branchId GROUP BY L.branchName;
 #SELECT name,address,Count(*) FROM tbl_book_loans NATURAL JOIN tbl_borrower GROUP BY cardNo HAVING count(*)> 5;
-#SELECT title,noOfCopies FROM ( ((tbl_book NATURAL JOIN tbl_author) NATURAL JOIN tbl_book_copies ) NATURAL JOIN tbl_library_branch) WHERE authorName = "Stephen King" AND branchName='Central';
+#SELECT * FROM ( ( (tbl_book B INNER JOIN tbl_author A ON B.authId = A.authorId ) NATURAL JOIN tbl_book_copies ) NATURAL JOIN tbl_library_branch) WHERE authorName = "Stephen King" AND branchName='Central';
